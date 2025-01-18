dae28aadeb772f158a96a5cc529ec80b2e5516af
Arlenne03 <arlenne.fierros@gmail.com>
2025-01-18 17:20:46 -0600

    Agregamos más gráficos en aplicación

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
 st.header("Análisis de datos de vehículos") # agregamos encabezado
 
-hist_button = st.button('Visualizar histograma del kilometraje') # crear un botón
+hist_button = st.button('Histograma del kilometraje') # crear un botón
         
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Histograma del kilometraje para el conjunto de datos de anuncios de venta de coches')
+    st.write('Diagrama del kilometraje para el conjunto de datos de anuncios de venta de coches')
             
     # crear un histograma
     fig = px.histogram(car_data, x="odometer", color_discrete_sequence=['indianred'])
@@ -19,11 +19,11 @@ if hist_button: # al hacer clic en el botón
     st.plotly_chart(fig, use_container_width=True)
     
     
-hist_button = st.button('Visualizar mapa de calor del kilometraje y los cilindros de los coches') # crear un botón
+hist_button = st.button('Mapa de calor del kilometraje y los cilindros de los coches') # crear un botón
 
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un mapa de calor de densidad del kilometraje y los cilindros de los coches')
+    st.write('Mapa de calor del kilometraje y los cilindros de los coches')
             
     # crear un histograma
     fig = px.density_heatmap(car_data, x="odometer", y="cylinders")
@@ -32,7 +32,7 @@ if hist_button: # al hacer clic en el botón
     st.plotly_chart(fig, use_container_width=True)
     
 
-build_scatter = st.checkbox('Visualizar diagrama de caja de los cilindros') # crear un botón
+build_scatter = st.checkbox('Diagrama de caja de los cilindros') # crear un botón
 
 if build_scatter: # al hacer clic en el botón
     # escribir un mensaje
@@ -45,7 +45,7 @@ if build_scatter: # al hacer clic en el botón
     st.plotly_chart(fig, use_container_width=True)
     
 
-build_scatter = st.checkbox('Visualizar un scatterplot del kilometraje y los precios') # crear un botón
+build_scatter = st.checkbox('Scatterplot del kilometraje y los precios') # crear un botón
   
 if build_scatter: # al hacer clic en el botón
     # escribir un mensaje
@@ -58,14 +58,27 @@ if build_scatter: # al hacer clic en el botón
     st.plotly_chart(fig, use_container_width=True)
 
 
-hist_button = st.button('Visualizar histograma de las condiciones de los coches') # crear un botón
+hist_button = st.button('Histograma de los tipos de combustible') # crear un botón
         
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Histograma de las condiciones de los vehículos en venta')
+    st.write('Diagrama de modelos de año y tipo de combustible usado')
             
     # crear un histograma
-    fig = px.histogram(car_data, x="condition", color_discrete_sequence=['royalblue'])
+    fig = px.histogram(car_data, x="model_year", color="fuel")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+    
+
+hist_button = st.button('Histograma de los modelos de año') # crear un botón
+        
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Diagrama de modelos de año y el tipo de vehículo en venta')
+            
+    # crear un histograma
+    fig = px.histogram(car_data, x="model_year", color="type")
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
